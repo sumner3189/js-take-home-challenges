2753ac2310e1dd376a1d81661d5410c58da0a0ab
sumner3189 <donidsumner2017@gmail.com>
2022-11-01 16:19:27 -0700

    day3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
-const currentDate = new Date();
-const getCurrentDay = currentDate.getDay();
-switch (getCurrentDay) {
-  case getCurrentDay === 0:
+
+//Day 2
+// const currentDate = new Date();
+// const getCurrentDay = currentDate.getDay();
+// switch (getCurrentDay) {
+//   case 0:
+//     console.log("sunday");
+//     break;
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("thats a incorrect number");
+//     break;
+// }
+
+//Day3
+// const colorOne = prompt("First Fav Color");
+// const colorTwo = prompt("second fav color");
+
+// const result = colorOne === 'blue'&& colorTwo==="grey"?"You chose my fav color": "try again"
+// console.log("🚀 ~ file: script.js ~ line 37 ~ result", result)
+
+const birthday = new Date("August 29, 1975 23:15:30"); 
+const dayOfweek = birthday.getDay()
+console.log("🚀 ~ file: script.js ~ line 41 ~ dayOfweek", dayOfweek)
+switch (dayOfweek) {
+  case 0:
     console.log("sunday");
     break;
   case 1:
